f15524965f6910e0474801b7b79b42c332eb964c
RudenkoSerhii <rudenko.sergey@ukr.net>
2022-11-13 19:03:20 +0200

    refactoring with locale storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,22 @@ import { Wrapper, H1, H2} from './Styled/Styled';
 
 export class App extends Component {
 state = {
-  contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+  contacts: [],
   filter: '',
 };
+
+componentDidMount() {
+this.setState({contacts: JSON.parse(localStorage.getItem('contacts'))})
+}
+
+componentDidUpdate(_, prevState) {
+
+if( this.state.contacts !== prevState.contacts) {
+localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+}
+
+}
+
 
 addContact = (contact) => {
   if (this.state.contacts.some(({ name }) => contact.name === name)) {
@@ -37,7 +45,7 @@ deleteContact = (id) => {
   render() {
 
     const normalizedFilter = this.state.filter.toLowerCase();
-    const visibleContacts = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+    const visibleContacts = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
 
     return (
       <Wrapper>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { App } from 'components/App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './components/theme';
+import contacts from 'contacts.json';
+
+localStorage.setItem('contacts', JSON.stringify(contacts));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
